69a4de9c884f0c8eb9c226752742f3091e0e369d
Sachin <great.sachin@gmail.com>
2021-02-13 09:13:02 +0530

    old doom config changes

--- doom/config.el
+++ doom/config.el
@@ -23,23 +23,13 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-solarized-light)
-
-;; If you intend to use org, it is recommended you change this!
-(setq org-directory "/mnt/e/Dropbox/org/")
-
+;;(setq doom-theme 'doom-solarized-light)
+(setq doom-theme 'doom-nord-light)
 
 ;; If you want to change the style of line numbers, change this to `relative' or
 ;; `nil' to disable it:
 (setq display-line-numbers-type 'relative)
 
-
-(add-hook 'org-agenda-mode-hook
-          (lambda ()
-	    ;; Navigate agenda
-	    (define-key org-agenda-mode-map (kbd "f") 'org-agenda-filter)
-	    (define-key org-agenda-mode-map (kbd "k") 'org-agenda-previous-item)
-	    (define-key org-agenda-mode-map (kbd "M-k") 'windmove-up)))
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
@@ -60,128 +50,58 @@
 ;(map! :n ",l" #'evil-window-right)
 ;(map! :n ",t" #'evil-window-top-left)
 ;(map! :n ",," #'evil-window-next)
-;(map! :n ",s" #'save-buffer)
+(map! :n ",w" #'save-buffer)
 ;(map! :n ",h" #'avy-goto-char-2)
 
 (setq ivy-re-builders-alist
-      '((t . ivy--regex-fuzzy)))
-
-(map! :map evil-org-agenda-mode-map :m "f" 'org-agenda-filter)
-(map! :map evil-org-agenda-mode-map :m "t" 'org-agenda-todo)
-(map! :map evil-org-agenda-mode-map :m "p" 'org-agenda-priority)
-
-(map! :map evil-org-agenda-mode-map :m "h" 'org-agenda-date-earlier-minutes)
-(map! :map evil-org-agenda-mode-map :m "l" 'org-agenda-date-later-minutes)
-
-(map! :map evil-org-agenda-mode-map :m "H" 'org-agenda-date-earlier)
-(map! :map evil-org-agenda-mode-map :m "L" 'org-agenda-date-later)
-
-(map! :map evil-org-agenda-mode-map :m "s" 'org-save-all-org-buffers)
-(map! :map evil-org-agenda-mode-map :m "r" 'org-agenda-redo-all)
-
-(map! :map evil-org-agenda-mode-map :m "n" 'org-agenda-next-item)
-(map! :map evil-org-agenda-mode-map :m "b" 'org-agenda-previous-item)
-
-(map! :map evil-org-agenda-mode-map :m "M-s" 'org-agenda-schedule)
-
-(map! :map evil-org-mode-map :m ",t" 'org-todo)
-
-(map! :map evil-org-mode-map :m "SPC m f" 'org-tags-sparse-tree)
-
-(map! :map evil-org-mode-map :m "M-m" 'org-tags-sparse-tree)
-
-(defun air-org-skip-subtree-if-habit ()
-  "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
-  (let ((subtree-end (save-excursion (org-end-of-subtree t))))
-    (if (string= (org-entry-get nil "STYLE") "habit")
-        subtree-end
-      nil)))
-(defun air-org-skip-subtree-if-priority (priority)
-  "Skip an agenda subtree if it has a priority of PRIORITY.
-PRIORITY may be one of the characters ?A, ?B, or ?C."
-  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
-        (pri-value (* 1000 (- org-lowest-priority priority)))
-        (pri-current (org-get-priority (thing-at-point 'line t))))
-    (if (= pri-value pri-current)
-        subtree-end
-      nil)))
-
-(setq org-agenda-custom-commands
-      '(("d" "Daily agenda and all TODOs"
-         (
-          (agenda "" ((org-agenda-ndays 1)))
-          (todo "NEXT"
-                ((org-agenda-sorting-strategy '(tag-up))
-                 (org-agenda-prefix-format " %i %-14t% s%?/T ")
-                 (org-agenda-overriding-header "Next items:")
-                 ))
-          ;((org-agenda-compact-blocks t))
-          ))))
-
-(defun air-pop-to-org-agenda (&optional split)
-  "Visit the org agenda, in the current window or a SPLIT."
-  (interactive "P")
-  (org-agenda nil "d")
-;;  (when (not split)
- ;;   (delete-other-windows)
-    )
-
-(define-key evil-normal-state-map (kbd "--") 'air-pop-to-org-agenda)
-;(define-key evil-insert-state-map (kbd "-") 'evil-normal-state)
-;(define-key evil-insert-state-map (kbd "ht") 'evil-normal-state)
+      '((t . ivy--regex-ignore-order)))
 
 (setq ivy-use-virtual-buffers t)
 (setq ivy-count-format "(%d/%d) ")
-
 
 (setq avy-keys '(?h ?t ?n ?s ?u ?e ?o ?d ?i))
 (setq avy-keys-alist
       `((avy-goto-char-2 . (?h ?t ?n ?s ?u ?e ?o ?d ?i))))
 (setq avy-all-windows t)
 
-(setq org-agenda-timegrid-use-ampm 1)
-
-(setq org-time-stamp-rounding-minutes (quote (0 15)))
-
-(customize-set-variable 'org-agenda-fontify-priorities t)
-(customize-set-variable 'org-priority-faces
-                        (quote ((?A :foreground "magenta3")
-                                (?B :foreground "SpringGreen4")
-                                (?C :foreground "darkorange3"))))
-(customize-set-variable 'org-default-priority ?C)
-
-(setq org-agenda-prefix-format '((agenda . " %i %-14t% s%?/T ")
-                                 (tags . " %i %-12:c")
-                                 (todo . " %i %-12:c")
-                                 (search . " %i %-12:c")))
-
-(defun sr/colorize-tags-with (col)
-  (interactive)
-  (goto-char (point-min))
-  (while (not (eobp))
-      (let (
-            (tag-end (or (search-forward "/")
-                         (point-at-bol))))
-        (if (not(eq tag-end (point-at-bol)))
-            (let (
-                  (tag-start (or (search-backward " ")
-                                 (point-at-eol))))
-              ;(delete-region (- tag-end 1) tag-end)
-              (add-text-properties (+ tag-start 1)
-                                   ;(+ tag-start 20)
-                                   tag-end
-                                   `(face (:foreground , col)))
-              )
-          () ))
-      (forward-line 1)
-      ))
-
-(defun sr/colorize-tags ()
-  (sr/colorize-tags-with "gray10"))
-; brown4
-; gray10
-
-(add-hook 'org-agenda-finalize-hook #'sr/colorize-tags)
-
-(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "SCHEDULED(s)" "WAIT(w@/!)" "HOLD(h@/!)" "|" "DONE(d!)" "CANCEL(c@)")
+(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAITING(w@/!)" "HOLD(h@/!)" "|" "DONE(d!)" "CANCELLED(c@)")
  (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")))
+
+;(setq key-chord-two-keys-delay 1)
+;(key-chord-define evil-insert-state-map "hh" 'evil-normal-state)
+;(key-chord-mode 1)
+
+;ap! :n ",h" #'evil-window-left)
+;(map! :n ",b" #'evil-window-bottom-right)
+;(map! :n ",l" #'evil-window-right)
+;(map! :n ",t" #'evil-window-top-left)
+;(map! :n ",," #'evil-window-next)
+(map! :n ",w" #'save-buffer)
+;(map! :n ",h" #'avy-goto-char-2)
+
+;(setq ivy-re-builders-alist
+ ;     '((t . ivy--regex-fuzzy)))
+
+(load-file "~/.doom.d/agenda.el")
+
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+
+(map! :leader
+  ;(:prefix-map ("f" . "file")
+ ;:desc "Find file"                   "f"   #'find-file
+ :desc "Find file"                   "."   #'counsel-fzf
+      )
+
+(setq avy-keys '(?h ?t ?n ?s ?u ?e ?o ?d ?i))
+(setq avy-keys-alist
+      `((avy-goto-char-2 . (?h ?t ?n ?s ?u ?e ?o ?d ?i))))
+(setq avy-all-windows t)
+
+(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAITING(w@/!)" "HOLD(h@/!)" "|" "DONE(d!)" "CANCELLED(c@)")
+ (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")))
+
+;(setq key-chord-two-keys-delay 1)
+;(key-chord-define evil-insert-state-map "hh" 'evil-normal-state)
+;(key-chord-mode 1)
+
---
+++ doom/custom.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#FDF6E3" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
+ '(custom-safe-themes
+   '("99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" default))
+ '(fci-rule-color "#504945")
+ '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#a89984"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#79740e"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#928374"))
+ '(objed-cursor-color "#9d0006")
+ '(org-agenda-fontify-priorities t)
+ '(package-selected-packages '(go-mode lsp-mode))
+ '(pdf-view-midnight-colors (cons "#282828" "#fbf1c7"))
+ '(rustic-ansi-faces
+   ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
+ '(tramp-encoding-shell "/bin/zsh")
+ '(vc-annotate-background "#fbf1c7")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#79740e")
+    (cons 40 "#8d7410")
+    (cons 60 "#a17512")
+    (cons 80 "#b57614")
+    (cons 100 "#b3620e")
+    (cons 120 "#b14e08")
+    (cons 140 "#af3a03")
+    (cons 160 "#af472e")
+    (cons 180 "#b0545a")
+    (cons 200 "#b16286")
+    (cons 220 "#aa415b")
+    (cons 240 "#a32030")
+    (cons 260 "#9d0006")
+    (cons 280 "#9a2021")
+    (cons 300 "#97413c")
+    (cons 320 "#946258")
+    (cons 340 "#504945")
+    (cons 360 "#504945")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
